23113daa7c3963bcc54adbf5d450537ec624fdc3
BobbyJoeSmith3 <lakota.sapa@gmail.com>
2019-03-02 14:35:24 -0500

    Orientation change add undercommons text

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// To listen for DeviceOrientationEvent, first check to see if the browser supports the events. Then, attach an event listener to the window object listening for deviceorientation events.
-console.log("connected");
-
 var orientation = window.screen.orientation;
 console.log(window.screen.height);
 window.addEventListener('resize', function() {
@@ -12,7 +9,9 @@ window.addEventListener('resize', function() {
 // Bind an event to window.orientationchange that, when the device is turned,
 // gets the orientation and displays it to on screen.
 $( window ).on( "orientationchange", function( event ) {
+  console.log("orientation has changed");
   $( "#orientation" ).text( "This device is in " + event.orientation + " mode!" );
+  $(".undercommons").toggle(400);
 });
 
 // You can also manually force this event to fire.
